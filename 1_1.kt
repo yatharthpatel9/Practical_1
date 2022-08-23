ce3bf456e8ab8b70ab807b497035d1d61255e115
yatharthpatel9 <93566991+yatharthpatel9@users.noreply.github.com>
2022-08-23 21:41:05 +0530

    Create 1_1.kt

---
+++ 1_1.kt
@@ -0,0 +1,24 @@
+fun main()
+{
+    // Defining Variables :
+    var i: Int = 22
+    var f: Float = 1.5F
+    var c: Char = 'T'
+    var st: String = "HMS"
+    var bl: Boolean = false
+    var d: Double = 96.36
+    var l: Long = 338927847
+    var s: Short = -2
+    var b: Byte = 127
+
+    // Printing all the Variables :
+    println("Integer Value: $i")
+    println("Float Value: $f")
+    println("Character Value: $c")
+    println("String Value: $st")
+    println("Boolean Value: $bl")
+    println("Double Value: $d")
+    println("Long Value: $l")
+    println("Short Value: $s")
+    println("Byte Value: $b")
+}
