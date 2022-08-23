2f291d0a626bc47a42fef36321df2167dcf74e67
yatharthpatel9 <93566991+yatharthpatel9@users.noreply.github.com>
2022-08-23 21:53:21 +0530

    Create 1_6.kt

---
+++ 1_6.kt
@@ -0,0 +1,10 @@
+fun calc(a:Int, b:Int,c:Int){
+    println("Addition of $a, $b, $c is ${a+b+c}")
+    println("Subtraction of $a, $b , $c is ${a-b-c}")
+    println("Multiplication of $a, $b , $c is ${a*b*c}")
+    println("Division of $b , $a is ${b/a}")
+}
+
+fun main(){
+    calc(111,2222,-222)
+}
