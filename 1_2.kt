173f9a29a87efd5fce598b791442787cefd23ec3
yatharthpatel9 <93566991+yatharthpatel9@users.noreply.github.com>
2022-08-23 21:43:39 +0530

    Create 1_2.kt

---
+++ 1_2.kt
@@ -0,0 +1,15 @@
+fun main(){
+    var a:Int = 10
+    var b = a.toDouble()
+    var g: String = "10"
+    var f: String = "11.12"
+    var c = Integer.parseInt(g)
+    var d = g.toInt()
+    var e = f.toDouble()
+    println("Integer Value:$a")
+    println("Double Value (From Integer):$b")
+    println("String Value:$g")
+    println("Integer Value1 (From String):$c")
+    println("Integer Value2 (From String):$d")
+    println("Double Value (From String):$e")
+}
